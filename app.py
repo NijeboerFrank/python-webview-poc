76f9010d0775435ebc97e5e82a0af9eb589576bf
NijeboerFrank <frank.nijeboer@gmail.com>
2021-12-03 10:21:49 +0100

    Add window code

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
+import time
+import threading
+
 import webview
 
-class Api:
 
+class Api:
     def do_something(self):
         print("Wow click from javascript")
+
+        print("Testing")
+
         return "Hello from python"
 
+    def hallo(self):
+        """Do some printing of lines and return a string"""
+        print("Testing")
+        return "Another hello function from Python"
+
+
+def show_alert(window):
+    time.sleep(5)
+    window.evaluate_js("alert('hoi')")
+
+
 api = Api()
-webview.create_window("Gekke App", "http://localhost:8080", js_api=api)
-webview.start(debug=True)
+window = webview.create_window("Gekke App", "vue-gui/dist/index.html", js_api=api)
+t = threading.Thread(target=show_alert, args=[window])
+t.start()
+
+webview.start()
+
+
+print("Done")
